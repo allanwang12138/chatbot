570896a777991ab7a24df7ba5c0ffde593950f83
allanwang12138 <65257199+allanwang12138@users.noreply.github.com>
2025-06-27 12:20:59 -0400

    Update streamlit_app.py - add import re

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ import datetime
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
+import re
 
 
 # ------------------- Load secrets -------------------
